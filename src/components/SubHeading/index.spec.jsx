5c11a2cca7d1605b1ce8fbb180e4434aa584e04b
Brenno Luan <brennoluan8530@gmail.com>
2026-05-19 16:50:58 -0300

    Add unit tests for SubHeading component

---
+++ src/components/SubHeading/index.spec.jsx
@@ -0,0 +1,28 @@
+import { render } from "@testing-library/react";
+import { SubHeading } from "./index.jsx";
+
+describe("SubHeading", () => {
+  describe("getBy", () => {
+    it("should render the subheading", () => {
+      const { getByText } = render(<SubHeading>To study</SubHeading>);
+      expect(getByText("To study")).toBeInTheDocument();
+    });
+
+    it.skip("should not render the subheading", () => {
+      const { getByText } = render(<SubHeading></SubHeading>);
+      expect(getByText("To study")).toBeNull();
+    });
+  });
+
+  describe("queryByText", () => {
+    it("should render the subheading", () => {
+      const { queryByText } = render(<SubHeading>To study</SubHeading>);
+      expect(queryByText("To study")).toBeInTheDocument();
+    });
+
+    it("should not render the subheading", () => {
+      const { queryByText } = render(<SubHeading></SubHeading>);
+      expect(queryByText("To study")).toBeNull();
+    });
+  });
+});
