315a72288157c02a5e35612a87648b9527757653
Brenno Luan <brennoluan8530@gmail.com>
2026-05-19 17:00:36 -0300

    Add dynamic description rendering logic to SubHeading component

--- src/components/SubHeading/index.jsx
+++ src/components/SubHeading/index.jsx
@@ -1,6 +1,22 @@
 import "./sub-heading.style.css";
+import { useEffect, useState } from "react";
 
 export function SubHeading({ children }) {
+  const [description, setDescription] = useState("");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setDescription("Adicione um novo item");
+    }, 500);
+  });
   if (!children) return <></>;
-  return <h2 className="subheading">{children}</h2>;
+
+  return (
+    <div>
+      <h2 className="subheading">
+        {children}
+        {!!description && <p>{description}</p>}
+      </h2>
+    </div>
+  );
 }
