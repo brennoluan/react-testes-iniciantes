2db581a6ec9da8e1fbecb6a72a4102458a250bef
Brenno Luan <brennoluan8530@gmail.com>
2026-05-19 20:45:27 -0300

    Enable `experimentalStudio` in Cypress configuration

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ export default defineConfig({
   allowCypressEnv: false,
 
   e2e: {
+    experimentalStudio: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
