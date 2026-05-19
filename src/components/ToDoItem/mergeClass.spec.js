558ab96cf3a1413cfabae72054d06421e9cd1a2f
Brenno Luan <brennoluan8530@gmail.com>
2026-05-19 14:46:27 -0300

    Simplify unit tests for mergeClass utility in ToDoItem component

--- src/components/ToDoItem/mergeClass.spec.js
+++ src/components/ToDoItem/mergeClass.spec.js
@@ -1,23 +1,11 @@
+import mergeClass from "./mergeClass.js";
+
 describe("./mergeClass.js", () => {
   it("should return the base class of the todo-item when the item is not completed", () => {
-    //Arrange
-    const isItemCompleted = false;
-
-    //Act
-    const styles = mergeClass(isItemCompleted);
-
-    //Assert
-    expect(styles).toBe("todo-item");
+    expect(mergeClass(false)).toBe("todo-item");
   });
 
   it("should return the base class of the todo-item when the item is completed", () => {
-    //Arrange
-    const isItemCompleted = true;
-
-    //Act
-    const styles = mergeClass(isItemCompleted);
-
-    //Assert
-    expect(styles).toBe("todo-item completed");
+    expect(mergeClass(true)).toBe("todo-item completed");
   });
 });
