1beebecb35cc824b9962249d221587a7c73e9e49
Brenno Luan <brennoluan8530@gmail.com>
2026-05-19 17:19:51 -0300

    Add user interaction test for FormToDo submission

--- src/components/FormToDo/index.spec.jsx
+++ src/components/FormToDo/index.spec.jsx
@@ -1,6 +1,7 @@
 import { render } from "@testing-library/react";
 import { TodoContext } from "../TodoProvider/TodoContext.js";
 import FormToDo from "./index.jsx";
+import userEvent from "@testing-library/user-event";
 
 describe("FormToDo", () => {
   it("should render the form", () => {
@@ -20,5 +21,24 @@ describe("FormToDo", () => {
     );
     expect(getByRole("textbox")).toBeInTheDocument();
     expect(getByRole("textbox")).toHaveValue("anything 2");
+  });
+
+  it("should ", async () => {
+    const fnSimulateSubmit = jest.fn();
+
+    const { getByRole } = render(
+      <TodoContext.Provider value={{ selectedTodo: { description: "anything 2" } }}>
+        <FormToDo onSubmit={fnSimulateSubmit} />
+      </TodoContext.Provider>,
+    );
+
+    const input = getByRole("textbox");
+    await userEvent.clear(input);
+    await userEvent.type(input, "anything 3");
+
+    const button = getByRole("button", { name: /salvar item/i });
+    await userEvent.click(button);
+
+    expect(fnSimulateSubmit).toHaveBeenCalled();
   });
 });
