302267032756b6a2fde81f5d1760d09fc023322a
Brenno Luan <brennoluan8530@gmail.com>
2026-05-19 20:07:53 -0300

    Add unit test for rendering count with items in `ToDoCount` component

--- src/components/ToDoCount/index.spec.jsx
+++ src/components/ToDoCount/index.spec.jsx
@@ -12,4 +12,15 @@ describe("ToDoCount", () => {
     const count = await findByText("0");
     expect(count).toBeInTheDocument();
   });
+
+  it("should render the count with items", async () => {
+    getTodos.mockResolvedValue([
+      { id: 1, description: "anything", completed: false, createdAt: Date.now() },
+      { id: 2, description: "anything", completed: true, createdAt: Date.now() },
+    ]);
+
+    const { findByText } = render(<ToDoCount />);
+    const count = await findByText("2");
+    expect(count).toBeInTheDocument();
+  });
 });
