e72eab2948b1f1008d485bb922bb233b0f141afe
Brenno Luan <brennoluan8530@gmail.com>
2026-05-19 14:58:55 -0300

    Refactor and parametrize unit tests for mergeClass utility in ToDoItem component

--- src/components/ToDoItem/mergeClass.spec.js
+++ src/components/ToDoItem/mergeClass.spec.js
@@ -8,4 +8,11 @@ describe("./mergeClass.js", () => {
   it("should return the base class of the todo-item when the item is completed", () => {
     expect(mergeClass(true)).toBe("todo-item completed");
   });
+
+  it.each([
+    { input: true, output: "todo-item completed" },
+    { input: false, output: "todo-item" },
+  ])("should return the base class of the todo-item", ({ input, output }) => {
+    expect(mergeClass(input)).toBe(output);
+  });
 });
